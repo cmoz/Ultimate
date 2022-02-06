bf987342eca9e25989a96c6251b7953f79f2967e
Christine Farion <cmoz@me.com>
2022-02-06 11:34:59 +0000

    Initial Commit
    Using the motion sensor Accelerometer on the Circuit Playground board, random function to randomise the colours of the NeoPixels, and choose random NeoPixels to light. Modified from the Twinkle Sketch on Adafruit.  https://learn.adafruit.com/circuit-playgrounds-motion-sensor/twinkle

---
+++ PlaygroundMotion/PlaygroundMotion.ino
@@ -0,0 +1,54 @@
+#include <Adafruit_CircuitPlayground.h>
+
+float X, Y, Z;
+#define MOVE_THRESHOLD 3
+
+void setup() {
+  Serial.begin(9600);
+  CircuitPlayground.begin();
+}
+
+void loop() {
+  CircuitPlayground.clearPixels();
+  X = CircuitPlayground.motionX();
+  Y = CircuitPlayground.motionY();
+  Z = CircuitPlayground.motionZ();
+ 
+   // Get the magnitude (length) of the 3 axis vector
+  // http://en.wikipedia.org/wiki/Euclidean_vector#Length
+  double storedVector = X*X;
+  storedVector += Y*Y;
+  storedVector += Z*Z;
+  storedVector = sqrt(storedVector);
+  Serial.print("Len: "); Serial.println(storedVector);
+  
+  // wait a bit
+  delay(100);
+  
+  // get new data!
+  X = CircuitPlayground.motionX();
+  Y = CircuitPlayground.motionY();
+  Z = CircuitPlayground.motionZ();
+  double newVector = X*X;
+  newVector += Y*Y;
+  newVector += Z*Z;
+  newVector = sqrt(newVector);
+  Serial.print("New Len: "); Serial.println(newVector);
+  
+  // are we moving 
+  if (abs(10*newVector - 10*storedVector) > MOVE_THRESHOLD) {
+    Serial.println("Twinkle!");
+
+    for (int x=0; x<10; x++){
+    int randomCol1 = random(255); 
+    int randomCol2 = random(255);
+    int randomCol3 = random(255);
+    int randomPos = random(10);
+    CircuitPlayground.setPixelColor(randomPos, randomCol1,  randomCol2,   randomCol3);
+
+    }
+  }
+  
+  delay(100);
+}
+
