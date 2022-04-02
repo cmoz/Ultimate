2476145dcae149fd1370b5dd285550b45f737615
Christine Farion <cmoz@me.com>
2022-04-02 17:01:52 +0100

    Update C6__UV_ColorVibration.ino

--- C6/C6__UV_ColorVibration/C6__UV_ColorVibration.ino
+++ C6/C6__UV_ColorVibration/C6__UV_ColorVibration.ino
@@ -14,6 +14,7 @@
  *             
  */
 
+
 #include <Adafruit_CircuitPlayground.h>
 #include "Adafruit_SI1145.h"
 
@@ -25,73 +26,78 @@ int vibePin = 1;
 void setup() {
 Serial.begin(9600);
 CircuitPlayground.begin();
-Serial.println("Adafruit SI1145 test");
 
-if (! uv.begin()) {
-Serial.println("Didn't find Si1145");
-while (1);
-}
-Serial.println("OK!");
-pinMode(vibePin, OUTPUT);
+  if (! uv.begin()) {
+  Serial.println("Didn't find Si1145");
+  while (1);
+  }
+  
+ pinMode(vibePin, OUTPUT);
 
 }
 
 void loop() {
-digitalWrite(vibePin, LOW);
-
-Serial.println("===================");
-
-float UVindex = uv.readUV();
-Serial.print("UV: "); Serial.println(UVindex);
-Serial.println(uv.readUV());
-
-  if(uv.readUV()>= UVthresholdIndoors && uv.readUV()<689 )
-  {
-  Serial.println("LOW");
-  neoColor(0,255,100);
+  digitalWrite(vibePin, LOW);
+  Serial.println("===================");
   
-  } else 
+  float UVindex = uv.readUV();
+  Serial.print("UV: "); Serial.println(UVindex);
+  Serial.println(uv.readUV());
 
-  if(uv.readUV()>=690 && uv.readUV()<1379)
-  {
-  Serial.println("Moderate");
-  CircuitPlayground.playTone(345, 200);
-  neoColor(255,191,0);
-  digitalWrite(vibePin, HIGH);
-  } else 
+    if(uv.readUV()>= UVthresholdIndoors && uv.readUV()<689 )
+    {
+      Serial.println("LOW");
+      neoColor(0,255,100);
+      digitalWrite(vibePin, LOW);
+    } else 
+  
+    if(uv.readUV()>=690 && uv.readUV()<1379) 
+    {
+      Serial.println("Moderate");
+      CircuitPlayground.playTone(345, 200);
+      neoColor(255,191,0);
+      vibrationWarn(100, 5);
+    } else 
+  
+     if(uv.readUV()>=1380  && uv.readUV()<1839)
+    {
+      Serial.println("High");
+      CircuitPlayground.playTone(445, 200);
+      neoColor(255,0,255);
+      vibrationWarn(200, 10);
+    } else
+  
+    if(uv.readUV()>=1839 && uv.readUV()<2529)
+    {
+      Serial.println("Very High");
+      CircuitPlayground.playTone(655, 400);
+      neoColor(244,30,30);
+      vibrationWarn(300, 15);
+    }
+  
+    if(uv.readUV()>=2530 && uv.readUV()<2760)
+    {
+      Serial.println("Extreme high");
+      CircuitPlayground.playTone(755, 600);
+      neoColor(255,0,0);
+      vibrationWarn(400, 20);
+    }
 
-   if(uv.readUV()>=1380 && uv.readUV()<1839)
-  {
-  Serial.println("High");
-  CircuitPlayground.playTone(445, 200);
-  neoColor(255,0,255);
-  digitalWrite(vibePin, HIGH);
-  } else
-
-  if(uv.readUV()>=1839 && uv.readUV()<2529)
-  {
-  Serial.println("Very High");
-  CircuitPlayground.playTone(655, 400);
-  neoColor(244,30,30);
-  digitalWrite(vibePin, HIGH);
-  }
-
-  if(uv.readUV()>=2530 && uv.readUV()<2760)
-  {
-  Serial.println("Extreme high");
-  CircuitPlayground.playTone(755, 600);
-  neoColor(255,0,0);
-  digitalWrite(vibePin, HIGH);
-  }
-
-delay(1000);
+  delay(1000);
 } 
 
-// a little function to change the NeoPixel colors 
-void neoColor(int r, int g, int b){
+void neoColor(int r, int g,int b){
   for (int neoPin = 0; neoPin <=10; neoPin++){
   CircuitPlayground.setPixelColor(neoPin,r,g,b);
   delay(20);
   }
-  
+}
+
+void vibrationWarn(int pulseDuration, int amount){
+  for (int loops = 0; loops <= amount; loops++){
+      digitalWrite(vibePin, HIGH);
+      delay(pulseDuration);
+      digitalWrite(vibePin, LOW);
+      delay(pulseDuration);
+  }
 }
