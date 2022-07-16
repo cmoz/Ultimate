a74ad91d61df97106535f8bb2c5538adfa498339
Christine Farion <cmoz@me.com>
2022-07-16 16:33:33 +0100

    Create Activity5_2.ino

---
+++ C5/Activity5_2.ino
@@ -0,0 +1,20 @@
+int sensorPin = 1;
+int ledPin = 2;
+
+void setup() {
+  pinMode(sensorPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int read = digitalRead(sensorPin);
+
+  if (read == LOW) {
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+  }
+
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+}
